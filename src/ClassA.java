edabce63af799b6cbac65447d329927350719e7f
Chaitali101095 <chaitu101095@gmail.com>
2023-10-04 21:52:29 +0530

    msg -my second commit

--- src/ClassA.java
+++ src/ClassA.java
@@ -1,4 +1,9 @@
 
 public class ClassA {
+	
+	public static void main(String[]arg )
+	{
+	System.out.println( "badal barsa bijoli");
+	}
 
 }
---
+++ src/ClassC.java
@@ -0,0 +1,9 @@
+
+public class ClassC {
+	
+	public static void main(String[]arg )
+	{
+	 System.out.println( "hello java 1111");
+	}
+
+}
